10c41b9903a6d4938a55664797355d11a64d11f4
delomian <delomian@gmail.com>
2021-11-08 15:22:57 +0200

    disable sharp due to glitch restrictions

--- adapters/fileAdapters/imageFileAdapter.js
+++ adapters/fileAdapters/imageFileAdapter.js
@@ -1,19 +1,20 @@
 const fileAdapter = require("../fileAdapter");
-const sharp = require("sharp");
+// const sharp = require("sharp");
 
 //TODO: make sure the loaded file is an image
 class imageFile extends fileAdapter {
+    //TODO: enable on a production server
     async resize({width, height}) {
-        let resizeParams = {};
-        if (typeof width === "number") {
-            resizeParams.width = width;
-        }
-        if (typeof height === "number") {
-            resizeParams.height = height;
-        }
-
-        const resizedImage = await sharp(this._fileData).resize(resizeParams);
-        this._fileData = await resizedImage.toBuffer();
+        // let resizeParams = {};
+        // if (typeof width === "number") {
+        //     resizeParams.width = width;
+        // }
+        // if (typeof height === "number") {
+        //     resizeParams.height = height;
+        // }
+        //
+        // const resizedImage = await sharp(this._fileData).resize(resizeParams);
+        // this._fileData = await resizedImage.toBuffer();
 
         return this;
     };
